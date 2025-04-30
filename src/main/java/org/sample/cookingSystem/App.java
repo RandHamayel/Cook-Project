af4516cafa8d11f73a36a5f46dc050f15cc34a88
RandHamayel <148476928+RandHamayel@users.noreply.github.com>
2025-04-30 10:27:34 +0300

    Your commit message here

---
+++ src/main/java/org/sample/cookingSystem/App.java
@@ -0,0 +1,13 @@
+package org.sample.cookingSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
